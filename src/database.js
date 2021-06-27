2a352dcc4b1df36a420b9e0825e64f03eeaf9c1a
ysr_dev <yobanisolisramirez@outlook.com>
2021-06-27 11:06:55 -0500

    Signup OK

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ mongoose.connect('mongodb://localhost/notes-db-app', {
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: false,
-        // useUnifiedTopology: true 
+        useUnifiedTopology: true
     })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
